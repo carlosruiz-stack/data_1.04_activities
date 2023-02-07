675e9659c76729b8dd5ebb06042c9a5d539468e7
carlosruiz-stack <122636522+carlosruiz-stack@users.noreply.github.com>
2023-02-07 19:49:52 +0100

    Add files via upload

---
+++ 1.04_activity_3_2.py
@@ -0,0 +1,11 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def function_2(a,b):
+    c=0
+    for i in range(a):
+        c+=2*b
+    return c
+    
+func2 = lambda a,b: 2*b
+print(func2(1,1))
